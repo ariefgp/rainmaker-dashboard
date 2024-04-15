bca3ba93941038a9fb2be1179238a8b8162c61f8
ariefgp <arief.gusti@gmail.com>
2024-04-15 20:52:41 +0700

    fix issue topbar and sidebar on login and signup

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,18 @@
 import Login from '../components/auth/Login';
+import RootLayout from '../layout';
 
 const LoginPage = () => {
     return (
-        <div className="flex justify-center items-center h-screen">
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <div className="card-body">
-                    <h2 className="card-title">Login</h2>
-                    <Login />
+        <RootLayout showNav={false}>
+            <div className="flex justify-center items-center h-screen">
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title">Login</h2>
+                        <Login />
+                    </div>
                 </div>
             </div>
-        </div>
+        </RootLayout>
     );
 };
 
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,15 +1,18 @@
 import SignUp from '../components/auth/SignUp';
+import RootLayout from '../layout';
 
 const SignUpPage = () => {
     return (
-        <div className="flex justify-center items-center h-screen">
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <div className="card-body">
-                    <h2 className="card-title">Sign Up</h2>
-                    <SignUp />
+        <RootLayout showNav={false}>
+            <div className="flex justify-center items-center h-screen">
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title">Sign Up</h2>
+                        <SignUp />
+                    </div>
                 </div>
             </div>
-        </div>
+        </RootLayout>
     );
 };
 
